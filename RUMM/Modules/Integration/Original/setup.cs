373752491d1c41fbdcd553deed23a00f849fc4c8
RichBread <royal.richbread@gmail.com>
2021-05-08 21:30:40 +0900

    setup.cs │ "setup"コマンドを追加

---
+++ RUMM/Modules/Integration/Original/setup.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using RUMM.Common;
+using System.Threading.Tasks;
+
+namespace RUMM.Modules.Integration.Original
+{
+    public class setup : ModuleBase<SocketCommandContext>
+    {
+        [Command ("setup")]
+
+        public async Task Setup()
+        {
+            string serverid = ((Context.Guild as SocketGuild).Id).ToString();
+
+            Method.Setup.CreateSetupFolder(serverid);
+
+            await Context.Channel.SendSuccessAsync("完了", "セットアップしたよ！");
+        }
+    }
+}
