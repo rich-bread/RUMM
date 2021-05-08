d0e350205498401fb26f1a8bc47af00935f38b6a
RichBread <royal.richbread@gmail.com>
2021-05-08 19:10:58 +0900

    フォルダー"Common" │ "\Task"を削除

---
+++ RUMM/Common/Setup.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Addons.Hosting;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace RUMM.Common
+{
+    public class Setup
+    {
+        private async Task CreateSetupFolder()
+        {
+
+        }
+    }
+}
