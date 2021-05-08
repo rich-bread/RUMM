5d30a4b03286d77575092d4d21ad76ab7516a890
RichBread <royal.richbread@gmail.com>
2021-05-08 17:25:00 +0900

    Program.csにコードを追加 / ProgramHandler.csを追加(必要なコードも)

--- RUMM/Program.cs
+++ RUMM/Program.cs
@@ -1,9 +1,64 @@
-﻿using System;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RUMM.Services;
 
 namespace RUMM
 {
     class Program
     {
+        static async Task Main()
+        {
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration(x =>
+                {
+                    var configuration = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", false, true)
+                        .Build();
 
+                    x.AddConfiguration(configuration);
+                })
+                .ConfigureLogging(x =>
+                {
+                    x.AddConsole();
+                    x.SetMinimumLevel(LogLevel.Information);
+                })
+                .ConfigureDiscordHost<DiscordSocketClient>((context, config) =>
+                {
+                    config.SocketConfig = new DiscordSocketConfig
+                    {
+                        LogLevel = LogSeverity.Verbose,
+                        AlwaysDownloadUsers = true,
+                        MessageCacheSize = 200,
+                    };
+
+                    config.Token = context.Configuration["token"];
+                })
+                .UseCommandService((context, config) =>
+                {
+                    config.CaseSensitiveCommands = false;
+                    config.LogLevel = LogSeverity.Verbose;
+                    config.DefaultRunMode = RunMode.Sync;
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHostedService<ProgramHandler>();
+                })
+                .UseConsoleLifetime();
+
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
     }
 }
