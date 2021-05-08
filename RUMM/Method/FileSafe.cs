dddcd1c952af3048f683aa334a2c949b362ae7a1
RichBread <royal.richbread@gmail.com>
2021-05-08 21:22:15 +0900

    セットアップ用のメソッドを作成

---
+++ RUMM/Method/FileSafe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace RUMM.Method
+{
+    public static class FileSafe
+    {
+        public static FileStream SafeCreateFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine(path + "already exists.");
+
+                return null;
+            }
+            return File.Create(path);
+        }
+
+        public static void SafeCreateFile_and_Write(string path, string text)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Could not create and write due to specified file exists.");
+            }
+            else
+            {
+                using (var file = File.Create(path))
+                {
+                    file.Close();
+                }
+                File.WriteAllText(path, text);
+
+                Console.WriteLine("Written successfully.");
+            }
+        }
+    }
+}
---
+++ RUMM/Method/Setup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace RUMM.Method
+{
+    public class Setup
+    {
+        public static void CreateSetupFolder(string id)
+        {
+            string serverfolder = $@"R:\Project\RUMM\.warehouse\{id}";
+
+            try
+            {
+                //『サーバー』フォルダー作成
+                DirectoryInfo mainfolder = new DirectoryInfo(serverfolder);
+                mainfolder.Create();
+
+                //「データ」フォルダー作成
+                DirectoryInfo datamain = mainfolder.CreateSubdirectory("Data");                         //「データ」全体フォルダー
+                DirectoryInfo data_recenter = datamain.CreateSubdirectory("Recenter");                  //「データ」中心座標フォルダー
+                DirectoryInfo data_trimmode = datamain.CreateSubdirectory("Trimmode");                  //「データ」切り取りモードフォルダー
+                DirectoryInfo data_area = datamain.CreateSubdirectory("Area");                          //「データ」エリアフォルダー
+
+                //「アップロード」フォルダー作成
+                DirectoryInfo uploadedmain = mainfolder.CreateSubdirectory("Uploaded");                 //「アップロード」全体フォルダー
+                DirectoryInfo uploadedmap = uploadedmain.CreateSubdirectory("UploadedMap");             //「アップロード」地図フォルダー(一時的にアップロードされたものを置いておく場所)
+
+                //「切り取り」フォルダー作成
+                DirectoryInfo trimedmain = mainfolder.CreateSubdirectory("Trimed");                     //「切り取り」全体フォルダー
+                DirectoryInfo trimedmap = trimedmain.CreateSubdirectory("TrimedMap");                   //「切り取り」地図フォルダー
+                DirectoryInfo trimedmap_x128 = trimedmain.CreateSubdirectory("TrimedMap[x128]");
+                DirectoryInfo trimedmap_x256 = trimedmain.CreateSubdirectory("TrimedMap[x256]");
+                DirectoryInfo trimedmap_backup = trimedmain.CreateSubdirectory("TrimedMap[Backup]");    //「切り取り」地図[バックアップ]フォルダー
+
+                //「完了」フォルダー作成
+                DirectoryInfo completedmain = mainfolder.CreateSubdirectory("Completed");               //「完了」全体フォルダー
+                DirectoryInfo completedmain_map = completedmain.CreateSubdirectory("CompletedMap");     //「完了」地図フォルダー
+                DirectoryInfo completedarea_map = completedmain.CreateSubdirectory("AreaMap");          //「完了」エリア地図フォルダー
+            }
+            catch (Exception reason)
+            {
+                Console.WriteLine(reason);
+            }
+
+            //データ用テキストファイルの宣言
+            string recenter_txt = $@"{serverfolder}\Data\Recenter\recenter.txt";
+            string trimmode_txt = $@"{serverfolder}\Data\Trimmode\trimmode.txt";
+
+            //中心座標変更テキストファイルの作成・書き込み
+            string recentercoord = "0\r\n0";
+            FileSafe.SafeCreateFile_and_Write(recenter_txt, recentercoord);
+
+            //切り取りモードテキストファイルの作成・書き込み
+            string defaulttrimmode = "0";
+            FileSafe.SafeCreateFile_and_Write(trimmode_txt, defaulttrimmode);
+        }
+    }
+}
