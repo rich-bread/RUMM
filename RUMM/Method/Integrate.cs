101ed6d43fed330d8140ad3146eb42e833d9eb2d
RichBread <royal.richbread@gmail.com>
2021-05-11 14:33:52 +0900

    エリア用の地図統合プログラムを追加

--- RUMM/Method/Integrate.cs
+++ RUMM/Method/Integrate.cs
@@ -125,5 +125,157 @@ namespace RUMM.Method
             bitmap.Save(compmap_path, System.Drawing.Imaging.ImageFormat.Png);
             bitmap.Dispose();
         }
+
+        public static void Original_Area(string area_text_path, string trimedmap_folder, string areacompmap_path)
+        {
+            List<string> coordslist = new List<string>();
+            List<int> xcoordslist = new List<int>();
+            List<int> zcoordslist = new List<int>();
+
+            try
+            {
+                StreamReader sr = new StreamReader(area_text_path);
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    coordslist.Add(line);
+                }
+                sr.Close();
+            }
+            catch
+            {
+            }
+
+            foreach (string precoord in coordslist)
+            {
+                string prexcoord = precoord.ToString().Split(',')[0];
+                string prezcoord = precoord.ToString().Split(',')[1];
+
+                xcoordslist.Add(int.Parse(prexcoord));
+                zcoordslist.Add(int.Parse(prezcoord));
+            }
+
+            int scale = 384;
+
+            int xcoordmax = xcoordslist.Max(); //Xの最大値
+            int xcoordmin = xcoordslist.Min(); //Xの最小値
+            int zcoordmax = zcoordslist.Max(); //Zの最大値
+            int zcoordmin = zcoordslist.Min(); //Zの最小値
+            int absxcoordmin = Math.Abs(xcoordmin); //X最小値の絶対値
+            int abszcoordmin = Math.Abs(zcoordmin); //Z最小値の絶対値
+
+            double precenterxcoord = Math.Truncate(((double)xcoordmax + (double)xcoordmin) / 2);
+            double precenterzcoord = Math.Truncate(((double)zcoordmax + (double)zcoordmin) / 2);
+            int centerxcoord = (int)precenterxcoord;
+            int centerzcoord = (int)precenterzcoord;
+
+            int prexcoordmaxsize = xcoordmax - Math.Abs(centerxcoord);
+            int prexcoordminsize = xcoordmin - Math.Abs(centerxcoord);
+            int prezcoordmaxsize = zcoordmax - Math.Abs(centerzcoord);
+            int prezcoordminsize = zcoordmin - Math.Abs(centerzcoord);
+            int xcoordsize = (prexcoordmaxsize + (prexcoordminsize * -1) + 1) * scale;
+            int zcoordsize = (prezcoordmaxsize + (prezcoordminsize * -1) + 1) * scale;
+
+            Bitmap bitmap = new Bitmap(xcoordsize, zcoordsize);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            g.TranslateTransform(Math.Abs(prexcoordminsize) * scale, Math.Abs(prezcoordminsize) * scale);
+
+            foreach (string mapcoord in coordslist)
+            {
+                int mapxcoord = int.Parse(mapcoord.Split(',')[0]);
+                int mapzcoord = int.Parse(mapcoord.Split(',')[1]);
+
+                int resizexcoord = mapxcoord - Math.Abs(centerxcoord);
+                int resizezcoord = mapzcoord - Math.Abs(centerzcoord);
+
+                Image image = Image.FromFile($@"{trimedmap_folder}\{mapxcoord},{mapzcoord}.png");
+
+                int fx = resizexcoord * scale;
+                int fz = resizezcoord * scale;
+
+                g.DrawImage(image, new Point(fx, fz));
+            }
+            g.Dispose();
+
+            bitmap.Save(areacompmap_path, System.Drawing.Imaging.ImageFormat.Png);
+            bitmap.Dispose();
+        }
+
+        public static void Designed_Realm3rd_Area(string area_text_path, string trimedmap_folder, string areacompmap_path)
+        {
+            List<string> coordslist = new List<string>();
+            List<int> xcoordslist = new List<int>();
+            List<int> zcoordslist = new List<int>();
+
+            try
+            {
+                StreamReader sr = new StreamReader(area_text_path);
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    coordslist.Add(line);
+                }
+                sr.Close();
+            }
+            catch
+            {
+            }
+
+            foreach (string precoord in coordslist)
+            {
+                string prexcoord = precoord.ToString().Split(',')[0];
+                string prezcoord = precoord.ToString().Split(',')[1];
+
+                xcoordslist.Add(int.Parse(prexcoord));
+                zcoordslist.Add(int.Parse(prezcoord) * -1);
+            }
+
+            int scale = 384;
+
+            int xcoordmax = xcoordslist.Max(); //Xの最大値
+            int xcoordmin = xcoordslist.Min(); //Xの最小値
+            int zcoordmax = zcoordslist.Max(); //Zの最大値
+            int zcoordmin = zcoordslist.Min(); //Zの最小値
+            int absxcoordmin = Math.Abs(xcoordmin); //X最小値の絶対値
+            int abszcoordmin = Math.Abs(zcoordmin); //Z最小値の絶対値
+
+            double precenterxcoord = Math.Truncate(((double)xcoordmax + (double)xcoordmin) / 2);
+            double precenterzcoord = Math.Truncate(((double)zcoordmax + (double)zcoordmin) / 2);
+            int centerxcoord = (int)precenterxcoord;
+            int centerzcoord = (int)precenterzcoord;
+
+            int prexcoordmaxsize = xcoordmax - Math.Abs(centerxcoord);
+            int prexcoordminsize = xcoordmin - Math.Abs(centerxcoord);
+            int prezcoordmaxsize = zcoordmax - Math.Abs(centerzcoord);
+            int prezcoordminsize = zcoordmin - Math.Abs(centerzcoord);
+            int xcoordsize = (prexcoordmaxsize + (prexcoordminsize * -1) + 1) * scale;
+            int zcoordsize = (prezcoordmaxsize + (prezcoordminsize * -1) + 1) * scale;
+
+            Bitmap bitmap = new Bitmap(xcoordsize, zcoordsize);
+            Graphics g = Graphics.FromImage(bitmap);
+
+            g.TranslateTransform(Math.Abs(prexcoordminsize) * scale, Math.Abs(prezcoordminsize) * scale);
+
+            foreach (string mapcoord in coordslist)
+            {
+                int mapxcoord = int.Parse(mapcoord.Split(',')[0]);
+                int mapzcoord = int.Parse(mapcoord.Split(',')[1]);
+
+                int resizexcoord = mapxcoord - Math.Abs(centerxcoord);
+                int resizezcoord = (mapzcoord * -1) - Math.Abs(centerzcoord);
+
+                Image image = Image.FromFile($@"{trimedmap_folder}\{mapxcoord},{mapzcoord}.png");
+
+                int fx = resizexcoord * scale;
+                int fz = resizezcoord * scale;
+
+                g.DrawImage(image, new Point(fx, fz));
+            }
+            g.Dispose();
+
+            bitmap.Save(areacompmap_path, System.Drawing.Imaging.ImageFormat.Png);
+            bitmap.Dispose();
+        }
     }
 }
